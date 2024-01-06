1dcdce5b470bdfeb10c2179f527f7e944a430606
ooooose <yuuki.oose1990@gmail.com>
2024-01-06 09:36:42 +0900

    make fix

---
+++ src/components/Banner.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import './Banner.scss';
+import { apiClient } from '../axios';
+import { requests } from '../request';
+
+type movieProps = {
+  title?: string;
+  name?: string;
+  original_name?: string;
+  backdrop_path?: string;
+  overview?: string;
+};
+
+export const Banner = () => {
+  const [movie, setMovie] = useState<movieProps>({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await apiClient.apiGet(requests.feachNetflixOriginals);
+      console.log(request.data.results);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.result.length - 1)
+        ],
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function truncate(str: any, n: number) {
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + '...' : str;
+    }
+  }
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: 'center center',
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom" />
+    </header>
+  );
+};
